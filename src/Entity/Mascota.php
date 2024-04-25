cad1c7da509035a299b6653ecaae12a091272871
CarmenSerbatic <mcgarcia@serbatic.es>
2024-04-25 08:59:16 +0200

    Inicializar repositorio Git

---
+++ src/Entity/Mascota.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MascotaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MascotaRepository::class)]
+class Mascota
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $edad = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $raza = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(?int $edad): static
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    public function getRaza(): ?string
+    {
+        return $this->raza;
+    }
+
+    public function setRaza(?string $raza): static
+    {
+        $this->raza = $raza;
+
+        return $this;
+    }
+}
